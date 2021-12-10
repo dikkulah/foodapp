cecfd88658cd42b62330334af5cc9aad644cdf60
dikkulah <dikkulah@gmail.com>
2021-12-10 22:43:44 +0300

    Arama Apissi düzeltildi ve kullanıldıı

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,31 +1,30 @@
-import React, {useState} from "react";
-import { View,Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../Components/SearchBar";
+import yelp from "../api/yelp";
 
+const SearchScreen = () => {
+  const [term, setTerm] = useState("");
+  const [restaurants, setRestaurants] = useState([]);
 
-const SearchScreen =() => {
-    const [term,setTerm]= useState('');
-
-return(
-    <View style={styles.background} >
-        <SearchBar 
-            term={term} 
-            onTermChange = {newTerm=>setTerm(newTerm)}
-            onTermSubmit ={()=> console.log(term,'submitted')}
-        />
-        <Text>{term} </Text>
+  const searchApi = async () => {
+    const response = await yelp.get("/search", {
+      params: {
+        limit: 50,
+        term,
+        location: "san jose",
+      }
+    });
+    setRestaurants(response.data.businesses);
+  };
+  return (
+    <View style={styles.background}>
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      <Text>We have found {restaurants.length} </Text>
     </View>
-
-
-
-
-)
+  );
 };
 
-const styles = StyleSheet.create({
-    background:{
-        backgroundColor:'white'
-    }
-});
+const styles = StyleSheet.create({});
 
 export default SearchScreen;
